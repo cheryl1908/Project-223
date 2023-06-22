b5616dd49c45cf4624a3317a37feaa72beaec975
Cheryl Fernandes <73298909+cheryl1908@users.noreply.github.com>
2023-06-22 19:06:42 +0530

    Add files via upload

--- password_genaerator.py
+++ password_genaerator.py
@@ -1,7 +1,3 @@
-
-
-#min_len = int(input("Enter minimum length of password : "))
-#max_len = int(input("Enter maximum length of pasword: "))
 counter=0
 characters = ['0','1','2','3','4','5','6','7','8','9',
                  'a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
